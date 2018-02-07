33ebef27fd056c94881afc551cf22b265206b8b1
Max Soe <maxsoe@gmail.com>
2018-02-07 15:35:22 +0000

    Testing retrieved data with local copy

--- core/allroutes.service.js
+++ core/allroutes.service.js
@@ -3,6 +3,7 @@ angular
   .module('allRoutesSvc', [])
   .service('allRoutesSvc', function($http) {
     this.getPosts = function() {
-      return $http.get('https://www.strava.com/api/v3/athletes/9149200/routes?access_token=b77ac5a33776a5ca8e16005ec66c3a962aeb66a0');
+      // return $http.get('https://www.strava.com/api/v3/athletes/9149200/routes?access_token=b77ac5a33776a5ca8e16005ec66c3a962aeb66a0');
+      return $http.get('core/localjson/local-copy.json');
     }
   });
